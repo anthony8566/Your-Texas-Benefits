21c0f5d9fb9c72cede328f0edcbca66c5fa00e33
anthony8566 <anthony856reyes@outlook.com>
2024-08-24 09:35:30 +0530

    Create src_components_Receipt.js

---
+++ src_components_Receipt.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Receipt() {
+  return (
+    <div className="receipt-container">
+      <div className="receipt-item">
+        <span>Total Expenses:</span>
+        <span>$300</span>
+      </div>
+    </div>
+  );
+}
+
+export default Receipt;
